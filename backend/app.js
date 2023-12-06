e0ea6621fe5a4c0f207cc0eca64477f15e85bd97
Selena <selena.k.rodriguez@gmail.com>
2023-12-06 14:27:39 -0600

    Added passport initialization.

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.status(path.join(_dirname, "public")));
-
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cors());
 
 app.use("/user", userRouter);
