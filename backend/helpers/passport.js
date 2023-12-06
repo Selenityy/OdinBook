ab4221ccf872bf9e0f64400d1d875e0fa3c13347
Selena <selena.k.rodriguez@gmail.com>
2023-12-06 14:28:09 -0600

    Created the passport authentication logic.

---
+++ backend/helpers/passport.js
@@ -0,0 +1,52 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/userModel");
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user, { message: "Logged in successfully" });
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+passport.use(
+  new JwtStrategy({
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.SESSION_SECRET,
+  }),
+  async (token, done) => {
+    try {
+      return done(null, token.user);
+    } catch (er) {
+      return done(err);
+    }
+  }
+);
