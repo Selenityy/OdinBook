c98222e32887fa92dc3ee97cdc7406ae28f3d9e9
Selena <selena.k.rodriguez@gmail.com>
2023-12-07 16:21:51 -0600

    Wrote out comments for the post and comment routes.

---
+++ backend/routes/postRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+
+/// Post Specific
+// get all posts (could be the same as feed on users)
+// get all likes on a post
+
+// create a post
+// update a post (optional)
+// delete a post
+// like a post
+
+/// Comment Specific (using postId as base route)
+// get all comments on a specific post
+// get a single comment on a post (optional)
+// get all likes on a comment
+
+// create a comment
+// update a comment (optional)
+// delete a comment
+// like a comment
+
+module.exports = router;
