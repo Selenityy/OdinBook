4601aba0550f585bc7551de4626b7eb6b7c8b192
Selena <selena.k.rodriguez@gmail.com>
2023-12-07 16:13:15 -0600

    Migrated this file to "userRoutes.js"

--- backend/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/user", (req, res, next) => {
-  res.send("this is a homepage");
-});
-
-module.exports = router;
