a6d5a8212be3c2801d3a495882edd15618381e06
Selena <selena.k.rodriguez@gmail.com>
2023-12-07 16:13:32 -0600

    Migrated user routes from "user.js" to "userRoutes.js"

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/user", (req, res, next) => {
+  res.send("this is a homepage");
+});
+
+/// User
+// user sign up
+// user log in
+// user log out
+
+// user get username
+// user update username
+// user get about 
+// user update about
+// user delete about
+// user get profile picture
+// user update profile picture
+// user delete account
+
+// user get feed
+// user create a post
+// user like a post
+// user create a comment
+// user like a comment
+
+/// Friends
+// user get friends list
+// user get friend request list
+// user request to be friends
+// user accept friend request
+// user reject friend request
+// user unfriend
+// user delete their own friend request
+
+module.exports = router;
