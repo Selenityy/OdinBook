c23425a35f9f329cd13d1ea9eb8be85893c2e9be
Selena <selena.k.rodriguez@gmail.com>
2023-12-07 15:15:02 -0600

    Created the post model to have comments, likes, timestamp and a body and optional images with an image schema.

---
+++ backend/models/postModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ImageSchema = new Schema({
+  imageUrl: { type: String },
+});
+
+const PostSchema = new Schema({
+  body: { type: String, required: true, minLength: 3, maxLength: 200 },
+  timestamp: { type: Date, default: Date.now },
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  likes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  likeCount: { type: Number, default: 0 },
+  images: [ImageSchema],
+});
+
+// get comments from a specific post
+PostSchema.virtual("comments", {
+  ref: "Comment",
+  localField: "_id",
+  foreignField: "postId",
+});
+
+// get comment count from a specific post
+PostSchema.virtual("commentCount", {
+  ref: "Comment",
+  localField: "_id",
+  foreignField: "postId",
+  count: true,
+});
+
+module.exports = mongoose.model("Post", PostSchema);
