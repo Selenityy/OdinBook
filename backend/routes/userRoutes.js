91fdbfdef46b7a060e276d59307aac6db7a7ef6b
Selena <selena.k.rodriguez@gmail.com>
2023-12-12 14:39:44 -0600

    Cleaned up commented out code.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,7 +7,6 @@ router.get("/user", (req, res, next) => {
   res.send("this is a homepage");
 });
 
-/// User
 // GET: User information
 router.get("/user/:userId", userController.getUserInfo);
 
@@ -28,20 +27,10 @@ router.put("/:userId/update/profilePic", userController.updateUserProfilePic);
 // DELETE: Deletions
 router.delete("/:userId/delete/account", userController.deleteUserAccount);
 
-/// Friends
-// user get friends list
-// user get friend request list
 // user request to be friends
 // user accept friend request
 // user reject friend request
 // user unfriend
 // user delete their own friend request
-
-// (below could be used in postRoutes instead of here)
-// user get feed
-// user create a post
-// user like a post
-// user create a comment
-// user like a comment
 
 module.exports = router;
