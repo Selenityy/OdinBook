5bb423b84062a59f95bfa534dce8aed1529cc300
Selena <selena.k.rodriguez@gmail.com>
2023-12-13 14:46:35 -0600

    Changed some route wording and added mergeParams for cleaner routing.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,34 +3,40 @@ const router = express.Router();
 const passport = require("passport");
 const userController = require("../controllers/userController");
 
-router.get("/user", (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.send("this is a homepage");
 });
 
 // GET: User information
-router.get("/user/:userId", userController.getUserInfo);
+router.get("/:userId", userController.getUserInfo);
 
 // POST: Authentications
-router.post("/user/signup", userController.signup);
+router.post("/signup", userController.signup);
 router.post(
-  "/user/login",
+  "/login",
   passport.authenticate("local", { session: false }),
   userController.login
 );
-router.post("/user/logout", userController.logout);
+router.get("/logout", userController.logout);
 
 // PUT: Updates
-router.put("/:userId/update/username", userController.updateUserUsername);
-router.put("/:userId/update/about", userController.updateUserAbout);
-router.put("/:userId/update/profilePic", userController.updateUserProfilePic);
+router.put("/:userId/username", userController.updateUserUsername);
+router.put("/:userId/about", userController.updateUserAbout);
+router.put(
+  "/:userId//profilePic",
+  userController.updateUserProfilePic
+);
 
 // DELETE: Deletions
-router.delete("/:userId/delete/account", userController.deleteUserAccount);
+router.delete("/:userId/account", userController.deleteUserAccount);
 
 // user request to be friends
 // user accept friend request
 // user reject friend request
 // user unfriend
 // user delete their own friend request
+
+const postRouter = require("./postRoutes");
+router.use("/:userId/posts", postRouter);
 
 module.exports = router;
