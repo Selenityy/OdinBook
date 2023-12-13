22d633d5e3204b6adae7a6dcc6226bd3f51e4965
Selena <selena.k.rodriguez@gmail.com>
2023-12-13 14:45:42 -0600

    Implemented create comment functionality.

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -20,3 +20,42 @@ exports.commentsOnAPost = asyncHandler(async (req, res, next) => {
   }
   res.json(postWithComments);
 });
+
+// Create a comment
+exports.createComment = [
+  // Validation middleware
+  body("body")
+    .trim()
+    .isLength({ min: 1, max: 200 })
+    .withMessage("The comment body cannot be empty"),
+
+  // Controller logic
+  asyncHandler(async (req, res, next) => {
+    const postId = req.params.postId;
+    const userId = req.params.userId;
+    const authenticatedUserId = req.user._id;
+
+    // Check for authorized user
+    if (userId !== authenticatedUserId.toString()) {
+      return res
+        .status(403)
+        .json({ message: "Unauthorized to create a comment" });
+    }
+
+    // check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const comment = new Comment({
+      body: req.body.body,
+      user: authenticatedUserId,
+      post: postId,
+    });
+    const savedComment = await comment.save();
+    res
+      .status(200)
+      .json({ comment: savedComment, message: "Comment created successfully" });
+  }),
+];
