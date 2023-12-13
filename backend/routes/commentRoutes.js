3c7eb6df64f10e1dd2d5eccf817e42b07a4d45cc
Selena <selena.k.rodriguez@gmail.com>
2023-12-13 14:46:05 -0600

    Created the base routes for creating, updating, deleting and fetching comments along with their likes.

--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -1,15 +1,16 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 const commentController = require("../controllers/commentController");
 
-/// Comment Specific (using postId as base route)
-// get all comments on a specific post
-router.get("/:postId/comments", commentController.commentsOnAPost);
+// GET: Fetching
+router.get("/", commentController.commentsOnAPost);
 
-// create a comment
-// update a comment (optional)
-// like a comment
-// delete a comment
-// delete all comments on a post
-// delete all likes on a post
-// delete all likes on a comment
+// POST: Creating
+router.post("/", commentController.createComment);
+
+// PUT: Updating
+router.put("/:commentId", commentController.updateComment);
+router.put("/:commentId/like", commentController.likeComment);
+
+// DELETE: Removing
+router.delete("/:commentId", commentController.deleteSpecificComment);
