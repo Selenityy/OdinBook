4f901f4364eaff46a5b185994d51d0cfc158aa49
Selena <selena.k.rodriguez@gmail.com>
2023-12-13 13:56:00 -0600

    Created functionality for retrieving all comments on a post, along with their like count.

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -3,3 +3,20 @@ const Post = require("../models/postModel");
 const Comment = require("../models/commentModel");
 const User = require("../models/userModel");
 const { body, validationResult } = require("express-validator");
+
+// Display comments on a post
+exports.commentsOnAPost = asyncHandler(async (req, res, next) => {
+  const postId = req.params.postId;
+  const postWithComments = await Post.findById(postId).populate({
+    path: "comments",
+    select: "body likeCount",
+    populate: {
+      path: "user",
+      select: "username profilePic",
+    },
+  });
+  if (!postWithComments) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+  res.json(postWithComments);
+});
