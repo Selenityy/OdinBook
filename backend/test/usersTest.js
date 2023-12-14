33f8783ac8ecc410c4f8ea4a4c81532391eb72fd
Selena <selena.k.rodriguez@gmail.com>
2023-12-14 16:06:15 -0600

    I implemented mongoMemoryServer into my user test

--- backend/test/usersTest.js
+++ backend/test/usersTest.js
@@ -1,18 +1,48 @@
 const userRoutes = require("../routes/userRoutes");
 const request = require("supertest");
 const express = require("express");
-const app = express();
 const passport = require("passport");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+
+const app = express();
 
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use("/", userRoutes);
+app.use("/user", userRoutes);
 
-describe("GET userRoutes connect to userController", () => {
-  it("responds with a 200", (done) => {
-    request(app).get("/").set("Accept", "application/json").expect(200, done);
+let mongoServer;
+
+// Set up MongoDB in-memory server
+before(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = mongoServer.getUri();
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+// Disconnect and stop server after tests
+after(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
+
+// User test
+describe("User Routes /signup", () => {
+  it("should sign up a new user", () => {
+    request(app)
+      .post("/signup")
+      .send({
+        username: "testuser",
+        password: "password123",
+        email: "test@example.com",
+        firstName: "Test",
+        lastName: "User",
+      })
+      .expect(201, "message", "User created successfully");
   });
 });
