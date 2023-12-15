6461f23dad8ab0f79cc091654e166e686d208311
Selena <selena.k.rodriguez@gmail.com>
2023-12-15 17:37:41 -0600

    Implemented additional tests for sign up endpoint

--- backend/__tests__/signup.test.js
+++ backend/__tests__/signup.test.js
@@ -5,8 +5,6 @@ const express = require("express");
 const passport = require("passport");
 const cors = require("cors");
 const path = require("path");
-const { hashPassword } = require("../helpers/bcrypt");
-const User = require("../models/userModel");
 const session = require("express-session");
 const db = require("../helpers/mongoConfigTesting");
 
@@ -33,13 +31,16 @@ beforeAll(() => {
   db.initializeMongoServer();
 });
 
+beforeEach(async () => {
+  await db.clearMongoServer();
+});
+
 // Disconnect and stop server after tests
 afterAll(async () => {
-  await db.clearMongoServer();
   await db.stopMongoServer();
 });
 
-// User test
+// Sign up test
 
 test("should sign up a new user", async () => {
   const res = await request(app).post("/user/signup").send({
@@ -50,23 +51,24 @@ test("should sign up a new user", async () => {
     lastName: "User",
   });
   expect(res.statusCode).toEqual(201);
+  expect(res.body.message).toEqual("User created successfully");
+});
 
-  // it("should login a user", async (done) => {
-  //   const hashedPassword = await hashPassword("password123");
-  //   // create a new user instance
-  //   const newUser = new User({
-  //     username: "testuser",
-  //     password: hashedPassword,
-  //     email: "test@example.com",
-  //     firstName: "Test",
-  //     lastName: "User",
-  //   });
-
-  //   request(app)
-  //     .post("/login")
-  //     .send({
-  //       user: newUser,
-  //     })
-  //     .expect(500, done);
-  // });
+test("should reject username for already existing", async () => {
+  const res = await request(app).post("/user/signup").send({
+    username: "testuser1",
+    password: "password123",
+    email: "test1@example.com",
+    firstName: "Test",
+    lastName: "User",
+  });
+  const res2 = await request(app).post("/user/signup").send({
+    username: "testuser1",
+    password: "password123",
+    email: "test1@example.com",
+    firstName: "Test",
+    lastName: "User",
+  });
+  expect(res2.statusCode).toEqual(400);
+  expect(res2.body.message).toEqual("Username or email already exists");
 });
