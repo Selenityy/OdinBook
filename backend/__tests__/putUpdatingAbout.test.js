7b35db6814f0575832b0420ae5c97f10a4ab1050
Selena <selena.k.rodriguez@gmail.com>
2023-12-21 16:10:19 -0600

    Created jest test for updating about section

---
+++ backend/__tests__/putUpdatingAbout.test.js
@@ -0,0 +1,77 @@
+require("dotenv").config();
+const userRouter = require("../routes/userRoutes");
+const request = require("supertest");
+const express = require("express");
+const passport = require("passport");
+const cors = require("cors");
+const path = require("path");
+const session = require("express-session");
+const db = require("../helpers/mongoConfigTesting");
+const User = require("../models/userModel");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+require("../helpers/passport");
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cors());
+
+app.use("/user", userRouter);
+
+let token = "";
+let userId = "";
+let user = "";
+let username = "";
+let about = "";
+
+// Set up MongoDB in-memory server
+beforeAll(() => {
+  db.initializeMongoServer();
+});
+
+beforeEach(async () => {
+  await db.clearMongoServer();
+  await request(app).post("/user/signup").send({
+    username: "testuser1",
+    password: "password123",
+    email: "test1@example.com",
+    firstName: "Test",
+    lastName: "User",
+  });
+
+  const response = await request(app).post("/user/login").send({
+    username: "testuser1",
+    password: "password123",
+  });
+  token = response.body.token;
+  user = await User.findOne({ username: "testuser1" });
+  userId = user._id;
+  username = user.username;
+  about = user.about;
+});
+
+// Disconnect and stop server after tests
+afterAll(async () => {
+  await db.stopMongoServer();
+});
+
+test("should update the about section", async () => {
+  const newAbout = "this is the updated about section";
+
+  const res = await request(app)
+    .put(`/user/${userId}/${username}/about`)
+    .send({ about: newAbout });
+  expect(res.statusCode).toEqual(200);
+  expect(res.body.message).toEqual("About section updated successfully");
+  expect(res.body.about).toEqual("this is the updated about section");
+});
