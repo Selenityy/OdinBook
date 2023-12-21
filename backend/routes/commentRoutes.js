56abc03b201dc5be7d578cbcef09bd23497c4b7f
Selena <selena.k.rodriguez@gmail.com>
2023-12-20 18:35:17 -0600

    Requires passport and authenticated in the route for comment creation

--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
+const passport = require("passport");
 const commentController = require("../controllers/commentController");
 
 // GET: Fetching
 router.get("/", commentController.commentsOnAPost);
 
 // POST: Creating
-router.post("/", commentController.createComment);
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  commentController.createComment
+);
 
 // PUT: Updating
 router.put("/:commentId", commentController.updateComment);
