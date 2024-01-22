364056a3d9d976424d0b610c216556b2c4c8a35c
Selena <selena.k.rodriguez@gmail.com>
2024-01-22 14:05:29 -0600

    Created the Login Page and Sign up Page with the components for fetching being imported in

---
+++ frontend/src/app/login/page.jsx
@@ -0,0 +1,12 @@
+import Login from "@/components/Login";
+
+const HomeLogIn = () => {
+  return (
+    <div>
+      <h2>Log in:</h2>
+      <Login />
+    </div>
+  );
+};
+
+export default HomeLogIn;
---
+++ frontend/src/app/signup/page.jsx
@@ -0,0 +1,12 @@
+import SignUp from "@/components/SignUp";
+
+const HomeSignUp = () => {
+  return (
+    <div>
+      <h2>Sign Up:</h2>
+      <SignUp />
+    </div>
+  );
+};
+
+export default HomeSignUp;
