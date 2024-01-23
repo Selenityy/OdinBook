3921463e32b6d6589633ec3ab3b08f9686a27b6b
Selena <selena.k.rodriguez@gmail.com>
2024-01-23 17:12:24 -0600

    Removed global Context and added state into the login, then once logged in updates the global Context user

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,28 +1,39 @@
 "use client";
 
+import { useState, useContext, useEffect } from "react";
 import { UserContext } from "@/context/Context";
-import React, { useContext } from "react";
-import Link from "next/link";
 
 const Login = () => {
-  const { userData, setUserData, handleLogin } = useContext(UserContext);
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const { user, setUser } = useContext(UserContext);
 
-  const apiFetchLogIn = async (username, password) => {
+  const apiFetchLogIn = async (formData) => {
     try {
       const res = await fetch("http://localhost:3000/user/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(userData),
+        body: JSON.stringify(formData),
       });
 
       if (res.ok) {
         const data = await res.json();
         const token = data.token;
+        const activeUser = data.user;
         localStorage.setItem("token", token);
         console.log("Login successful:", data);
-        handleLogin(userData);
+        // setUserData global
+        setUser((prevState) => ({
+          ...prevState,
+          ...activeUser,
+          isLoggedIn: true,
+        }));
+        console.log("activeUser:", activeUser);
+        // redirect to User Home Page
       } else {
         console.log(res);
         console.error("Login failed:", res.statusText);
@@ -32,10 +43,13 @@ const Login = () => {
     }
   };
 
+  useEffect(() => {
+    console.log(user)
+  }, [user]);
+
   const handleLoginSubmit = async (e) => {
     e.preventDefault();
-    await apiFetchLogIn();
-    setUserData({});
+    await apiFetchLogIn(formData);
   };
 
   return (
@@ -47,9 +61,9 @@ const Login = () => {
             type="text"
             id="username"
             name="username"
-            value={userData.username || ""}
+            value={formData.username || ""}
             onChange={(e) =>
-              setUserData({ ...userData, username: e.target.value })
+              setFormData({ ...formData, username: e.target.value })
             }
             required
           />
@@ -60,9 +74,9 @@ const Login = () => {
             type="password"
             id="password"
             name="password"
-            value={userData.password || ""}
+            value={formData.password || ""}
             onChange={(e) =>
-              setUserData({ ...userData, password: e.target.value })
+              setFormData({ ...formData, password: e.target.value })
             }
             required
           />
@@ -71,9 +85,6 @@ const Login = () => {
           Login
         </button>
       </form>
-      <div>
-        <Link href="/signup">Do not have an account yet? Sign up here.</Link>
-      </div>
     </div>
   );
 };
