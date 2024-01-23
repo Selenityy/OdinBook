015c4d574010b08235a6141be189a34f0ea3461b
Selena <selena.k.rodriguez@gmail.com>
2024-01-23 17:12:39 -0600

    Migrated the logout from the global Context to this component

--- frontend/src/components/Logout.jsx
+++ frontend/src/components/Logout.jsx
@@ -1,5 +1,21 @@
+"use client";
+
+import { UserContext } from "@/context/Context";
+import { useContext } from "react";
+
 const Logout = () => {
-  console.log("inside the logout");
+  const { setUser } = useContext(UserContext);
+  setUser({
+    fullname: "",
+    email: "",
+    username: "",
+    about: "",
+    profilePic: "",
+    friends: [],
+    friendRequests: [],
+    posts: [],
+    isLoggedIn: false,
+  });
   localStorage.removeItem("token");
 };
 
