a1d51c8e0201a6a9eb5583dbd4bb9568dfd013d8
Selena <selena.k.rodriguez@gmail.com>
2024-01-24 17:31:53 -0600

    Added the functionality to show the profile picture and name if logged in

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -2,13 +2,18 @@
 
 // if logged in, show the username and profile pic
 // if not logged in, show sign up button
+
+// username
+// profile picture
+// profile pic drops down to select profile, logout
+
 import { useContext, useEffect } from "react";
 import { UserContext } from "@/context/Context";
+import Image from "next/image";
 
 const UserProfile = () => {
-  const { userData } = useContext(UserContext);
-  const { username, profilePic, isLoggedIn } = userData;
-  console.log("userData:", userData);
+  const { user } = useContext(UserContext);
+  const { username, profilePic, isLoggedIn } = user;
 
   const handleClick = () => {
     // drops down menu
@@ -18,12 +23,15 @@ const UserProfile = () => {
     <div>
       {isLoggedIn ? (
         <>
-          <button
+          <Image
             className="header"
             id="profile-pic"
-            src={profilePic}
+            src={`http://localhost:3000${profilePic}`}
+            alt="profile-pic"
+            width={50}
+            height={50}
             onClick={handleClick}
-          ></button>
+          />
           <div>{username}</div>
         </>
       ) : (
