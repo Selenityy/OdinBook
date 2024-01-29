454b6e1c0d6ea3775de70d92d5af9bdf93aa2911
Selena <selena.k.rodriguez@gmail.com>
2024-01-29 15:33:14 -0600

    Added a useEffect to get the user Id from the jwt token and set the user data in the global context

--- frontend/src/context/context.jsx
+++ frontend/src/context/context.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
 
 export const UserContext = createContext({});
 
@@ -20,6 +20,39 @@ export const UserProvider = ({ children }) => {
     testUser: false,
     isLoggedIn: false,
   });
+
+  // useEffect(() => {
+  //   console.log("context user:", user);
+  // }, [user]);
+
+  useEffect(() => {
+    const fetchUserData = async (token) => {
+      try {
+        const response = await fetch(`http://localhost:3000/user/data`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        });
+        if (response.ok) {
+          const userData = await response.json();
+          setUser({
+            ...userData,
+            isLoggedIn: true,
+          });
+          console.log("in useEffect setUser");
+        }
+      } catch (error) {
+        console.error("Failed to fetch user data:", error);
+      }
+    };
+
+    const token = localStorage.getItem("token");
+    if (token) {
+      fetchUserData(token);
+    }
+  }, []);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
