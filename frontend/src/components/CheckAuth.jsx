43aa2fbab81ce8cb1335fd97221d755ff50d95c0
Selena <selena.k.rodriguez@gmail.com>
2024-02-02 16:52:15 -0600

    Added a router.push to the home page if not logged in

--- frontend/src/components/CheckAuth.jsx
+++ frontend/src/components/CheckAuth.jsx
@@ -3,15 +3,19 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchUserData } from "@/redux/features/user-slice";
+import { useRouter } from "next/navigation";
 
 const CheckAuth = () => {
   const dispatch = useDispatch();
+  const router = useRouter();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
       dispatch(fetchUserData());
+    } else {
+      router.push("/");
     }
-  }, [dispatch]);
+  }, [dispatch, router]);
 };
 export default CheckAuth;
