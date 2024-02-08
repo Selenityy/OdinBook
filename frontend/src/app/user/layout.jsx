cbb8438eb48bc1fde1fe944e67ef5e524df06212
Selena <selena.k.rodriguez@gmail.com>
2024-02-08 15:46:00 -0600

    Gave user layout a grid layout

--- frontend/src/app/user/layout.jsx
+++ frontend/src/app/user/layout.jsx
@@ -1,12 +1,25 @@
+import FriendReqList from "@/components/FriendReqList";
 import Header from "@/components/Header";
+import NavSideBar from "@/components/NavSideBar";
+import "../../styles/globals.css";
 
 const UserLayout = async ({ children }) => {
   return (
-    <>
-      <Header />
-      <div>(User Layout)</div>
-      <main>{children}</main>
-    </>
+    <div className="my-6 mx-16 grid grid-areas-userLayout grid-cols-userLayout grid-rows-userLayout h-screen gap-3">
+      <section className="grid-in-header">
+        <Header />
+      </section>
+      <section className="grid-in-user">
+        <NavSideBar />
+      </section>
+      <section className="grid-in-nav">
+        <NavSideBar />
+      </section>
+      <main className="grid-in-main">{children}</main>
+      <section className="grid-in-friends">
+        <FriendReqList />
+      </section>
+    </div>
   );
 };
 
