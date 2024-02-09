693d700a07d7ea980dabeaf48e5c9a70ceec9d16
Selena <selena.k.rodriguez@gmail.com>
2024-02-09 17:13:32 -0600

    Added an iteration of friends to a ul list

--- frontend/src/components/FriendReqList.jsx
+++ frontend/src/components/FriendReqList.jsx
@@ -3,12 +3,30 @@
 import { useSelector, useDispatch } from "react-redux";
 
 const FriendReqList = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
+  const userState = useSelector((state) => state.user);
+  const friends = userState.value.friends; // is given as an array of ids
+  //   console.log(friends);
 
   return (
     <div>
-      <h1>Friend Requests</h1>
-      <p>Friend List</p>
+      <div>
+        <h1 className="font-semibold">Friend Requests</h1>
+      </div>
+      <div>
+        <h1 className="font-semibold">Friend List</h1>
+        <div>
+          {friends && friends.length > 0 ? (
+            <ul>
+              {friends.map((friend) => (
+                <li key={friend._id}>{friend.username}</li>
+              ))}
+            </ul>
+          ) : (
+            <p>No friends to display.</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
