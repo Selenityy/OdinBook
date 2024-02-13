4043bd22ae47935a9bb590054848ade4defaa178
Selena <selena.k.rodriguez@gmail.com>
2024-02-13 14:31:51 -0600

    Added create post form and friend req component to the page

--- frontend/src/app/user/page.jsx
+++ frontend/src/app/user/page.jsx
@@ -1,7 +1,19 @@
+import CreatePostForm from "@/components/CreatePostForm";
+import FriendReqList from "@/components/FriendReqList";
+import UserFeed from "@/components/UserFeed";
+
 const UserHomePage = () => {
   return (
-    <div>
-      <h2>User Feed</h2>
+    <div className="grid grid-cols-[1fr_0.5fr] grid-rows-[auto_1fr] gap-x-6 auto-row-auto h-screen w-full">
+      <section className="col-start-1 row-start-1 flex justify-center mb-8">
+        <CreatePostForm />
+      </section>
+      <section className="col-start-1 row-start-2 flex justify-center">
+        <UserFeed />
+      </section>
+      <section className="col-start-2 row-span-2">
+        <FriendReqList />
+      </section>
     </div>
   );
 };
