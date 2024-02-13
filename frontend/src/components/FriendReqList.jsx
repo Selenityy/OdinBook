161af2c724666f94ecffed2302147f04631b8c0c
Selena <selena.k.rodriguez@gmail.com>
2024-02-13 14:32:09 -0600

    Added a ul and li for friend requests, same as friends

--- frontend/src/components/FriendReqList.jsx
+++ frontend/src/components/FriendReqList.jsx
@@ -5,17 +5,28 @@ import { useSelector, useDispatch } from "react-redux";
 const FriendReqList = () => {
   // const dispatch = useDispatch();
   const userState = useSelector((state) => state.user);
-  const friends = userState.value.friends; // is given as an array of ids
-  //   console.log(friends);
+  const friends = userState.value.friends;
+  const friendRequests = userState.value.friendRequests;
 
   return (
-    <div>
+    <div className="flex flex-col border-2 border-black rounded-lg p-2 gap-3">
       <div>
         <h1 className="font-semibold">Friend Requests</h1>
+        <div className="indent-5">
+          {friendRequests && friendRequests.length > 0 ? (
+            <ul>
+              {friendRequests.map((friendReq) => (
+                <li key={friendReq._id}>{friendReq.username}</li>
+              ))}
+            </ul>
+          ) : (
+            <div>No friend requests to display.</div>
+          )}
+        </div>
       </div>
       <div>
         <h1 className="font-semibold">Friend List</h1>
-        <div>
+        <div className="indent-5">
           {friends && friends.length > 0 ? (
             <ul>
               {friends.map((friend) => (
@@ -23,7 +34,7 @@ const FriendReqList = () => {
               ))}
             </ul>
           ) : (
-            <p>No friends to display.</p>
+            <div>No friends to display.</div>
           )}
         </div>
       </div>
