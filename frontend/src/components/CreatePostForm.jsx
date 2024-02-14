e2e8661707f27fac4c8717de63c735754f3f9440
Selena <selena.k.rodriguez@gmail.com>
2024-02-14 16:32:39 -0600

    Fixed post creation background to be rounded along with it's parent div

--- frontend/src/components/CreatePostForm.jsx
+++ frontend/src/components/CreatePostForm.jsx
@@ -16,7 +16,7 @@ const CreatePostForm = () => {
 
   return (
     <div className="w-full mx-16 flex flex-col gap-6">
-      <form onSubmit={onPostCreationClick} className="bg-slate-100">
+      <form onSubmit={onPostCreationClick} className="bg-slate-100 rounded-lg">
         <label htmlFor="post-creation-content"></label>
         <div className="flex flex-col p-1 border-2 border-black rounded-lg">
           <div>
@@ -24,7 +24,7 @@ const CreatePostForm = () => {
               type="text"
               id="post-creation-content"
               name="post-creation-content"
-              className="text-base px-2 pt-2 pb-7 w-full"
+              className="text-base px-2 pt-2 pb-10 w-full bg-slate-50"
               placeholder="What's on your mind..."
               required
             />
