0fc07117915334af4883621de0a792fee9449982
Selena <selena.k.rodriguez@gmail.com>
2024-02-14 16:33:23 -0600

    Wrote to do list for this component in commented code

--- frontend/src/components/UserFeed.jsx
+++ frontend/src/components/UserFeed.jsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { useSelector } from "react-redux";
-import CreatePostForm from "./CreatePostForm";
 
 const UserFeed = () => {
   const userState = useSelector((state) => state.user);
+  // check user's post, and display them
+  // check user's friends
+  // check those friend's posts and display them
+  // refresh anytime a new post is created by user or friends
 
   return (
     <div className="w-full mx-16 flex flex-col gap-6 auto-row-auto">
