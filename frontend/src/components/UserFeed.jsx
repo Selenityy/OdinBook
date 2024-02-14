3d73b05bccf35c378aeeebf06461a9ce13167420
Selena <selena.k.rodriguez@gmail.com>
2024-02-14 17:22:40 -0600

    Retrieved user and friend posts from the dispatch of fetchUserFeedPosts

--- frontend/src/components/UserFeed.jsx
+++ frontend/src/components/UserFeed.jsx
@@ -1,13 +1,21 @@
 "use client";
 
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchUserFeedPosts } from "@/redux/features/user-slice";
+import { useEffect } from "react";
 
 const UserFeed = () => {
+  const dispatch = useDispatch();
   const userState = useSelector((state) => state.user);
-  // check user's post, and display them
-  // check user's friends
-  // check those friend's posts and display them
-  // refresh anytime a new post is created by user or friends
+  const userId = userState.value._id;
+  const userPostArray = userState.value.userPosts;
+  const friendsPostArray = userState.value.friendsPosts;
+  console.log("user", userPostArray);
+  console.log("friends", friendsPostArray);
+
+  useEffect(() => {
+    dispatch(fetchUserFeedPosts(userId));
+  }, [dispatch, userId]);
 
   return (
     <div className="w-full mx-16 flex flex-col gap-6 auto-row-auto">
