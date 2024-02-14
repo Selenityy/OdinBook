f52bb69bf30591b701753d8b057cd0b3f7bdcc90
Selena <selena.k.rodriguez@gmail.com>
2024-02-14 16:32:54 -0600

    Created skeleton on clicks for the accept and reject buttons for friend requests

--- frontend/src/components/FriendReqList.jsx
+++ frontend/src/components/FriendReqList.jsx
@@ -9,15 +9,26 @@ const FriendReqList = () => {
   const friends = userState.value.friends;
   const friendRequests = userState.value.friendRequests;
 
+  const onAcceptClick = () => {
+    console.log("accepted");
+  };
+
+  const onRejectClick = () => {
+    console.log("rejected");
+  };
+
   return (
-    <div className="flex flex-col border-2 border-black rounded-lg p-2 gap-3">
+    <div className="flex flex-col rounded-lg p-2 gap-3 bg-slate-50">
       <div>
-        <h1 className="font-semibold">Friend Requests</h1>
+        <h1 className="font-semibold text-gray-800">Friend Requests</h1>
         <div className="mx-5 my-3">
           {friendRequests && friendRequests.length > 0 ? (
             <ul>
               {friendRequests.map((friendReq) => (
-                <li key={friendReq._id} className="flex items-center space-x-3">
+                <li
+                  key={friendReq._id}
+                  className="flex justify-center items-center space-x-3"
+                >
                   <div className="w-10 h-10 relative">
                     <Image
                       src={`http://localhost:3000${friendReq.profilePic}`}
@@ -28,45 +39,49 @@ const FriendReqList = () => {
                       priority
                     />
                   </div>
-                  <div>{friendReq.username}</div>
+                  <div className="text-gray-800">{friendReq.username}</div>
+                  <div onClick={onAcceptClick}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={2.5}
+                      stroke="currentColor"
+                      className="w-4 h-4"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m4.5 12.75 6 6 9-13.5"
+                      />
+                    </svg>
+                  </div>
+                  <div onClick={onRejectClick}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={2.5}
+                      stroke="currentColor"
+                      className="w-4 h-4"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18 18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </div>
                 </li>
               ))}
             </ul>
           ) : (
-            <div>No friend requests to display.</div>
+            <div></div>
           )}
         </div>
       </div>
-      {/* <div className="indent-5">
-          {friendRequests && friendRequests.length > 0 ? (
-            <ul>
-              {friendRequests.map((friendReq) => (
-                <>
-                  <li key={friendReq._id}>{friendReq.profilePic}</li>
-                  <li key={friendReq._id}>{friendReq.username}</li>
-                </>
-              ))}
-            </ul>
-          ) : (
-            <div>No friend requests to display.</div>
-          )}
-        </div> */}
-      {/* <div>
-        <h1 className="font-semibold">Friend List</h1>
-        <div className="indent-5">
-          {friends && friends.length > 0 ? (
-            <ul>
-              {friends.map((friend) => (
-                <li key={friend._id}>{friend.username}</li>
-              ))}
-            </ul>
-          ) : (
-            <div>No friends to display.</div>
-          )}
-        </div>
-      </div> */}
       <div>
-        <h1 className="font-semibold">Friends</h1>
+        <h1 className="font-semibold text-gray-800">Friends</h1>
         <div className="mx-5 my-3">
           {friends && friends.length > 0 ? (
             <ul>
@@ -82,12 +97,12 @@ const FriendReqList = () => {
                       priority
                     />
                   </div>
-                  <div>{friend.username}</div>
+                  <div className="text-gray-800">{friend.username}</div>
                 </li>
               ))}
             </ul>
           ) : (
-            <div>No friend requests to display.</div>
+            <div></div>
           )}
         </div>
       </div>
