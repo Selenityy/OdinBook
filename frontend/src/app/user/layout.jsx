188a4b1dcfb168547c74ef8c864285ca4dbb9dbb
Selena <selena.k.rodriguez@gmail.com>
2024-02-15 17:59:21 -0600

    Removed NavUser temporarily for a better UI

--- frontend/src/app/user/layout.jsx
+++ frontend/src/app/user/layout.jsx
@@ -6,14 +6,14 @@ import "../../styles/globals.css";
 
 const UserLayout = ({ children }) => {
   return (
-    <div className="grid grid-areas-userLayout grid-cols-userLayout grid-rows-userLayout h-screen gap-y-4 gap-x-6">
+    <div className="grid grid-areas-userLayout grid-cols-userLayout grid-rows-userLayout h-screen gap-y-4">
       <section className="py-3 grid-in-header border-b-2 border-slate-50/[0.2]">
         <Header />
       </section>
-      <section className="grid-in-user ml-[138px]">
+      {/* <section className="grid-in-user ml-[138px]">
         <NavUser />
-      </section>
-      <section className="grid-in-nav ml-[138px]">
+      </section> */}
+      <section className="grid-in-user ml-[138px]">
         <NavSideBar />
       </section>
       <main className="grid-in-main mr-[138px]">{children}</main>
