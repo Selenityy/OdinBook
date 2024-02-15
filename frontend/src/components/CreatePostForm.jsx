521f055bff7135c1f90ac20614dee9f8acbe1fe6
Selena <selena.k.rodriguez@gmail.com>
2024-02-15 17:59:47 -0600

    Edited the border and background colors

--- frontend/src/components/CreatePostForm.jsx
+++ frontend/src/components/CreatePostForm.jsx
@@ -15,22 +15,25 @@ const CreatePostForm = () => {
   };
 
   return (
-    <div className="w-full mx-16 flex flex-col gap-6">
-      <form onSubmit={onPostCreationClick} className="bg-slate-100 rounded-lg">
+    <div className="w-full flex flex-col gap-6">
+      <form
+        onSubmit={onPostCreationClick}
+        className="bg-slate-700 border border-slate-500"
+      >
         <label htmlFor="post-creation-content"></label>
-        <div className="flex flex-col p-1 border-2 border-black rounded-lg">
+        <div className="flex flex-col p-1 bg-slate-700 border border-slate-500">
           <div>
             <input
               type="text"
               id="post-creation-content"
               name="post-creation-content"
-              className="text-base px-2 pt-2 pb-10 w-full bg-slate-50"
+              className="text-lg px-2 pt-2 pb-10 w-full bg-slate-700 text-white"
               placeholder="What's on your mind..."
               required
             />
           </div>
           <div className="flex justify-end p-2">
-            <button id="post-btn" type="submit" className="btn w-fit">
+            <button id="post-btn" type="submit" className="btn2 w-fit">
               Post
             </button>
           </div>
