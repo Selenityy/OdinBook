7fae66e8be2e3d4f553e1c13c2a8d4eb1e131f15
Selena <selena.k.rodriguez@gmail.com>
2024-02-15 18:00:00 -0600

    Edited the text colors

--- frontend/src/components/NavUser.jsx
+++ frontend/src/components/NavUser.jsx
@@ -15,7 +15,7 @@ const NavUser = () => {
   const friendLength = friends.length;
 
   return (
-    <div className="rounded-lg grid grid-cols-3 grid-rows-3 grid-flow-row w-fit p-2.5 gap-2 items-center bg-slate-50">
+    <div className="rounded-lg grid grid-cols-3 grid-rows-3 grid-flow-row w-fit p-2.5 gap-2 items-center bg-slate-700">
       <div className="col-start-1 row-span-2 row-start-1">
         <Image
           className="header"
@@ -28,17 +28,17 @@ const NavUser = () => {
         />
       </div>
       <div className="col-start-2 col-span-2 row-start-1 m-px flex items-end justify-center items-end">
-        <div className="font-semibold text-lg text-gray-800">{username}</div>
+        <div className="font-semibold text-lg text-white">{username}</div>
       </div>
-      <div className="col-start-2 row-start-2 flex gap-1 text-xs w-fit text-gray-800">
+      <div className="col-start-2 row-start-2 flex gap-1 text-xs w-fit text-white">
         <div>{postLength}</div>
         <div>Posts</div>
       </div>
-      <div className="col-start-3 row-start-2 flex gap-1 text-xs w-fit text-gray-800">
+      <div className="col-start-3 row-start-2 flex gap-1 text-xs w-fit text-white">
         <div>{friendLength}</div>
         <div>Friends</div>
       </div>
-      <div className="col-start-1 col-span-3 row-start-3 items-center text-base py-2 text-gray-800">
+      <div className="col-start-1 col-span-3 row-start-3 items-center text-base py-2 text-white">
         <div>{about}</div>
       </div>
     </div>
