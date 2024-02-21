c7039ae56b7ba4c429103a5234fc6367207c94b0
Selena <selena.k.rodriguez@gmail.com>
2024-02-21 16:53:16 -0600

    Added state for confirming password, if the passwords match and if the user is created. Implemented a div for if the passwords don't match or if the user was created. No longer redirects

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -13,89 +13,151 @@ const SignUp = () => {
     username: "",
     password: "",
   });
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordMismatch, setPasswordMismatch] = useState(false);
+  const [userCreated, setUserCreated] = useState(false);
   const dispatch = useDispatch();
   const router = useRouter();
 
   const onClickSignUp = async (e) => {
     e.preventDefault();
-    dispatch(signUpUser(formData));
-    setFormData({
-      firstName: "",
-      lastName: "",
-      email: "",
-      username: "",
-      password: "",
-    });
-    router.push("/login");
+    if (formData.password !== confirmPassword) {
+      setPasswordMismatch(true);
+    } else {
+      setPasswordMismatch(false);
+      dispatch(signUpUser(formData));
+      setUserCreated(true);
+      setFormData({
+        firstName: "",
+        lastName: "",
+        email: "",
+        username: "",
+        password: "",
+      });
+      setConfirmPassword("");
+      // router.push("/");
+    }
   };
 
   return (
-    <div>
-      <form onSubmit={onClickSignUp}>
-        <p>
-          <label htmlFor="firstName">First Name:</label>
+    <div className="w-96">
+      <form className="w-full grid" onSubmit={onClickSignUp}>
+        <div className="pb-3 flex gap-2">
+          <label htmlFor="firstName" className="text-gray-700 text-lg">
+            First Name:
+          </label>
           <input
             type="text"
             id="firstName"
             name="firstName"
-            value={formData.firstName}
+            className="text-gray-800 text-lg indent-1 flex grow"
+            value={formData.firstName || ""}
             onChange={(e) =>
               setFormData({ ...formData, firstName: e.target.value })
             }
+            required
           />
-        </p>
-        <p>
-          <label htmlFor="lastName">Last Name:</label>
+        </div>
+        <div className="pb-3 flex gap-2">
+          <label htmlFor="lastName" className="text-gray-700 text-lg">
+            Last Name:
+          </label>
           <input
             type="text"
             id="lastName"
             name="lastName"
-            value={formData.lastName}
+            className="text-gray-800 text-lg indent-1 flex grow"
+            value={formData.lastName || ""}
             onChange={(e) =>
               setFormData({ ...formData, lastName: e.target.value })
             }
+            required
           />
-        </p>
-        <p>
-          <label htmlFor="email">Email:</label>
+        </div>
+        <div className="pb-3 flex gap-2">
+          <label htmlFor="email" className="text-gray-700 text-lg">
+            Email:
+          </label>
           <input
             type="email"
             id="email"
             name="email"
-            value={formData.email}
+            className="text-gray-800 text-lg indent-1 flex grow"
+            value={formData.email || ""}
             onChange={(e) =>
               setFormData({ ...formData, email: e.target.value })
             }
+            required
           />
-        </p>
-        <p>
-          <label htmlFor="username">Username:</label>
+        </div>
+        <div className="pb-3 flex gap-2">
+          <label htmlFor="username" className="text-gray-700 text-lg">
+            Username:
+          </label>
           <input
             type="username"
             id="username"
             name="username"
-            value={formData.username}
+            className="text-gray-800 text-lg indent-1 flex grow"
+            value={formData.username || ""}
             onChange={(e) =>
               setFormData({ ...formData, username: e.target.value })
             }
+            required
           />
-        </p>
-        <p>
-          <label htmlFor="password">Password:</label>
+        </div>
+        <div className="pb-3 flex gap-2">
+          <label htmlFor="password" className="text-gray-700 text-lg">
+            Password:
+          </label>
           <input
             type="password"
             id="password"
             name="password"
-            value={formData.password}
-            onChange={(e) =>
-              setFormData({ ...formData, password: e.target.value })
-            }
+            className={`text-gray-800 text-lg indent-1 flex grow ${
+              passwordMismatch ? "border border-red-500" : ""
+            }`}
+            value={formData.password || ""}
+            onChange={(e) => {
+              setFormData({ ...formData, password: e.target.value });
+              setPasswordMismatch(false);
+            }}
+            required
           />
-        </p>
-        <button id="sign-up-btn" type="submit">
+        </div>
+        <div className="pb-3 flex gap-2">
+          <label htmlFor="confirmPassword" className="text-gray-700 text-lg">
+            Confirm Password:
+          </label>
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            className={`text-gray-800 text-lg indent-1 flex grow ${
+              passwordMismatch ? "border border-red-500" : ""
+            }`}
+            value={confirmPassword || ""}
+            onChange={(e) => {
+              setConfirmPassword(e.target.value);
+              setPasswordMismatch(false);
+            }}
+            required
+          />
+        </div>
+        {passwordMismatch && (
+          <div className="text-red-500 text-sm italic">
+            *Passwords do not match
+          </div>
+        )}
+        <button id="sign-up-btn" type="submit" className="btn">
           Sign Up
         </button>
       </form>
+      {userCreated && (
+        <div className=" pt-2 text-green-500 text-md font-bold">
+          Account creation successful!
+        </div>
+      )}
     </div>
   );
 };
