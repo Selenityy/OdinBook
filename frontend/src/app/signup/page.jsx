3e4bc38cae103ac7a3dd5a2399d595fc51426728
Selena <selena.k.rodriguez@gmail.com>
2024-02-21 16:52:12 -0600

    Added a duplicate page of the home page

--- frontend/src/app/signup/page.jsx
+++ frontend/src/app/signup/page.jsx
@@ -3,10 +3,29 @@ import Link from "next/link";
 
 const HomeSignUp = () => {
   return (
-    <div>
-      <h2>Sign Up:</h2>
-      <SignUp />
-      <Link href="/login">Already have an account? Log in here.</Link>
+    <div className="grid grid-areas-homeLayout grid-cols-homeLayout grid-rows-homeLayout h-screen items-center bg-stone-100">
+      <section className="grid-in-odin flex flex-col items-center">
+        <h1 className="text-8xl font-semibold text-gray-800">OdinBook</h1>
+        <h2 className="text-2xl font-light text-gray-600">
+          Connect with others
+        </h2>
+      </section>
+      <div className="grid-in-line border border-gray-500 h-full"></div>
+      <section className="grid-in-login flex flex-col items-center justify-center h-full">
+        <SignUp />
+        <br></br>
+        <div className="flex items-center justify-center space-x-2 w-96">
+          <div className="w-full border border-gray-500"></div>
+          <span className="text-gray-700 text-lg">or</span>
+          <div className="w-full border border-gray-500"></div>
+        </div>
+        <br></br>
+        <Link href="/" className="">
+          <button type="button" className="btn w-96">
+            Log In
+          </button>
+        </Link>
+      </section>
     </div>
   );
 };
