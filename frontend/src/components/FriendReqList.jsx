6923eb012a91b6667f22bd4dc858ec8fa93e6642
Selena <selena.k.rodriguez@gmail.com>
2024-02-22 16:44:25 -0600

    Fetched all users Id and filtered out the friends, friendRequests and current to display Possible People You May Know showing the remaining users

--- frontend/src/components/FriendReqList.jsx
+++ frontend/src/components/FriendReqList.jsx
@@ -2,12 +2,42 @@
 
 import { useSelector, useDispatch } from "react-redux";
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
 const FriendReqList = () => {
-  // const dispatch = useDispatch();
+  //   const dispatch = useDispatch();
   const userState = useSelector((state) => state.user);
   const friends = userState.value.friends;
   const friendRequests = userState.value.friendRequests;
+  const [allUsers, setAllUsers] = useState([]);
+
+  // Extract Ids
+  const friendIds = friends.map((friend) => friend._id);
+  const friendRequestsId = friendRequests.map((request) => request._id);
+  const currentUserId = userState.value._id;
+
+  // Filter allUsers removing the friends, friend requests and current user
+  const filteredUsers = allUsers.filter(
+    (user) =>
+      !friendIds.includes(user._id) &&
+      !friendRequestsId.includes(user._id) &&
+      user._id !== currentUserId
+  );
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(`http://localhost:3000/user/all`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const allUsersObj = await res.json();
+      setAllUsers(allUsersObj);
+    }
+
+    fetchData();
+  }, []);
 
   const onAcceptClick = () => {
     console.log("accepted");
@@ -15,6 +45,10 @@ const FriendReqList = () => {
 
   const onRejectClick = () => {
     console.log("rejected");
+  };
+
+  const onFriendRequestClick = () => {
+    console.log("friend requested");
   };
 
   return (
@@ -33,9 +67,9 @@ const FriendReqList = () => {
                     <Image
                       src={`http://localhost:3000${friendReq.profilePic}`}
                       alt={`${friendReq.username}'s profile picture`}
-                      layout="fill"
-                      objectFit="cover"
-                      className="rounded-full"
+                      fill
+                      className="rounded-full object-cover"
+                      sizes="100%"
                       priority
                     />
                   </div>
@@ -43,11 +77,10 @@ const FriendReqList = () => {
                   <div onClick={onAcceptClick}>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
                       viewBox="0 0 24 24"
                       strokeWidth={2.5}
                       stroke="currentColor"
-                      className="w-4 h-4 text-white"
+                      className="w-4 h-4 text-white fill-none"
                     >
                       <path
                         strokeLinecap="round"
@@ -59,11 +92,10 @@ const FriendReqList = () => {
                   <div onClick={onRejectClick}>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
                       viewBox="0 0 24 24"
                       strokeWidth={2.5}
                       stroke="currentColor"
-                      className="w-4 h-4 text-white"
+                      className="w-4 h-4 text-white fill-none"
                     >
                       <path
                         strokeLinecap="round"
@@ -91,13 +123,56 @@ const FriendReqList = () => {
                     <Image
                       src={`http://localhost:3000${friend.profilePic}`}
                       alt={`${friend.username}'s profile picture`}
-                      layout="fill"
-                      objectFit="cover"
-                      className="rounded-full"
+                      fill
+                      className="rounded-full object-cover"
+                      sizes="100%"
                       priority
                     />
                   </div>
                   <div className="text-white">{friend.username}</div>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </div>
+      <div>
+        <h1 className="font-bold tracking-wide text-white">
+          People You May Know
+        </h1>
+        <div className="mx-5 my-3">
+          {filteredUsers && filteredUsers.length > 0 ? (
+            <ul>
+              {filteredUsers.map((user) => (
+                <li key={user._id} className="flex items-center space-x-3">
+                  <div className="w-10 h-10 relative">
+                    <Image
+                      src={`http://localhost:3000${user.profilePic}`}
+                      alt={`${user.username}'s profile picture`}
+                      fill
+                      className="rounded-full object-cover"
+                      sizes="100%"
+                      priority
+                    />
+                  </div>
+                  <div className="text-white">{user.username}</div>
+                  <div onClick={onFriendRequestClick}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.75}
+                      stroke="currentColor"
+                      className="w-5 h-5 text-white fill-none"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
+                      />
+                    </svg>
+                  </div>
                 </li>
               ))}
             </ul>
