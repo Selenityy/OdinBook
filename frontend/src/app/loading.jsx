44aaf92b6fd23da6dbdfdaa180964841c9059ac3
Selena <selena.k.rodriguez@gmail.com>
2024-02-22 14:33:42 -0600

    Added styles to the loading page

--- frontend/src/app/loading.jsx
+++ frontend/src/app/loading.jsx
@@ -1,9 +1,18 @@
 const Loading = () => {
-    return (
-        <div>
-            <h2>Loading...</h2>
-        </div>
-    )
+  return (
+    <div className="grid grid-areas-homeLayout grid-cols-homeLayout grid-rows-homeLayout h-screen items-center bg-stone-100">
+      <section className="grid-in-odin flex flex-col items-center">
+        <h1 className="text-8xl font-semibold text-gray-800">OdinBook</h1>
+        <h2 className="text-2xl font-light text-gray-600">
+          Connect with others
+        </h2>
+      </section>
+      <div className="grid-in-line border border-gray-500 h-full"></div>
+      <section className="grid-in-login flex flex-col items-center justify-center h-full">
+        <div className="text-xl italic font-semibold text-gray-800">Loading...</div>
+      </section>
+    </div>
+  );
 };
 
 export default Loading;
