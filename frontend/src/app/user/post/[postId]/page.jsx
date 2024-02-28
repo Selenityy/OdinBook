0641b6bbb545b549c5ad4f6cef6922d024b9290f
Selena <selena.k.rodriguez@gmail.com>
2024-02-28 15:19:49 -0600

    Added tailwindcss to match the userFeed home page

--- frontend/src/app/user/post/[postId]/page.jsx
+++ frontend/src/app/user/post/[postId]/page.jsx
@@ -1,18 +1,17 @@
-// import UniquePost from "@/components/UniquePost";
-
 import UniquePost from "@/components/UniquePost";
+import "../../../../styles/globals.css";
 
 const UniquePostPage = () => {
   return (
-    <div>
-      <section>
+    <div className="w-full"> 
+      <section className="flex items-center gap-2 mb-3">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="w-6 h-6"
+          className="w-6 h-6 text-white"
         >
           <path
             strokeLinecap="round"
@@ -20,9 +19,11 @@ const UniquePostPage = () => {
             d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
           />
         </svg>
-        <h1>Post</h1>
+        <h1 className="text-white text-xl font-semibold">Post</h1>
       </section>
-      <UniquePost />
+      <section>
+        <UniquePost />
+      </section>
       <section>(form for posting comment, probably a component)</section>
       <section>(list out the comments, probably a comment component)</section>
     </div>
