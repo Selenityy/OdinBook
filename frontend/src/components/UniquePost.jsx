2580cf36f65f8cad2c19c6826904ac4eb46e5c1e
Selena <selena.k.rodriguez@gmail.com>
2024-02-28 13:48:18 -0600

    Created the functionality for the unique post page to reload and re-fetch the post's comments upon triggered

--- frontend/src/components/UniquePost.jsx
+++ frontend/src/components/UniquePost.jsx
@@ -1,14 +1,46 @@
 "use client";
 
 import { useSelector, useDispatch } from "react-redux";
-import { fetchUserFeedPosts, likePost } from "@/redux/features/user-slice";
+import {
+  fetchUserFeedPosts,
+  likePost,
+  fetchUniquePost,
+} from "@/redux/features/user-slice";
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 const UniquePost = () => {
-  return (
-    <div>Unique Post Component</div>
-  )
-}
+  const dispatch = useDispatch();
+  const pathname = usePathname();
+  const segments = pathname.split('/');
+  const postId = segments.pop();
+  const userState = useSelector((state) => state.user.value);
+  const userId = userState._id;
+  const userPostArray = userState.userPosts || [];
+  const friendsPostArray = userState.friendPosts || [];
+  const allPosts = [...userPostArray, ...friendsPostArray];
+  const sortedPosts = allPosts.sort(
+    (a, b) => new Date(b.timestamp) - new Date(a.timestamp)
+  );
+  const [refreshDataTrigger, setRefreshDataTrigger] = useState(false);
 
-export default UniquePost
+  useEffect(() => {
+    const updatePostComments = async () => {
+      try {
+        await dispatch(fetchUniquePost({userId, postId})).unwrap();
+      } catch (error) {
+        console.error("Failed to update post comments:", error);
+      }
+    };
+    updatePostComments();
+  }, [dispatch, refreshDataTrigger, userId, postId]);
+
+  return (
+    <div>
+      <section>(actual post, like & comment count)</section>
+    </div>
+  );
+};
+
+export default UniquePost;
