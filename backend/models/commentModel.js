9652ff223c854db7b1ca1018c690ad551abbc3ed
Selena <selena.k.rodriguez@gmail.com>
2024-03-05 15:11:33 -0600

    Added comment and comment count to the Comment model, for comment nesting

--- backend/models/commentModel.js
+++ backend/models/commentModel.js
@@ -1,18 +1,26 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const CommentSchema = new Schema({
-  body: { type: String, minLength: 1, maxLength: 50, required: true },
-  timestamp: { type: Date, default: Date.now },
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
-  likes: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
-  likeCount: { type: Number, default: 0 },
-});
+const CommentSchema = new Schema(
+  {
+    body: { type: String, minLength: 1, maxLength: 50, required: true },
+    timestamp: { type: Date, default: Date.now },
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    likeCount: { type: Number, default: 0 },
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+    commentCount: { type: Number, default: 0 },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 module.exports = mongoose.model("Comment", CommentSchema);
