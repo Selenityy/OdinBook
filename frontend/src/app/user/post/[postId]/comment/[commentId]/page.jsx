25a3c9bb8d4fd4f3fe946f09eb53af81dea5f12a
Selena <selena.k.rodriguez@gmail.com>
2024-03-05 16:14:49 -0600

    Updated to hold components with css

--- frontend/src/app/user/post/[postId]/comment/[commentId]/page.jsx
+++ frontend/src/app/user/post/[postId]/comment/[commentId]/page.jsx
@@ -1,5 +1,53 @@
+"use client";
+
+import CreateCommentForm from "@/components/CreateCommentForm";
+import UniqueComment from "@/components/UniqueComment";
+import UniqueCommentComments from "@/components/UniqueCommentComments";
+import "../../../../../../styles/globals.css";
+
 const UniqueCommentPage = () => {
-  return <div>UniqueCommentPage</div>;
+  return (
+    <div className="w-full">
+      <section className="flex items-center gap-2 mb-3">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 text-white"
+          //   onClick={() => onBackClick()}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+          />
+        </svg>
+        <h1 className="text-white text-xl font-semibold">Comment</h1>
+      </section>
+      <section>
+        <UniqueComment />
+      </section>
+      <section>
+        <CreateCommentForm
+          refreshDataTrigger={refreshDataTrigger}
+          setRefreshDataTrigger={setRefreshDataTrigger}
+          postId={postId}
+          userId={userId}
+        />
+      </section>
+      <section>
+        <UniqueCommentComments />
+        {/* <UniquePostComments
+          userId={userId}
+          commentsArray={commentsArray}
+          setRefreshDataTrigger={setRefreshDataTrigger}
+          postId={postId}
+        /> */}
+      </section>
+    </div>
+  );
 };
 
 export default UniqueCommentPage;
