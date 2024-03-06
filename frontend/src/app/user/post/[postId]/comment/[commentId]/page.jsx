a99455429c24afa011db0e0835ee62ed74ca737c
Selena <selena.k.rodriguez@gmail.com>
2024-03-06 16:08:43 -0600

    Added pathname and onBackClick to the page

--- frontend/src/app/user/post/[postId]/comment/[commentId]/page.jsx
+++ frontend/src/app/user/post/[postId]/comment/[commentId]/page.jsx
@@ -3,9 +3,25 @@
 import CreateCommentForm from "@/components/CreateCommentForm";
 import UniqueComment from "@/components/UniqueComment";
 import UniqueCommentComments from "@/components/UniqueCommentComments";
+import { useRouter, usePathname } from "next/navigation";
+import { useDispatch, useSelector } from "react-redux";
 import "../../../../../../styles/globals.css";
 
 const UniqueCommentPage = () => {
+  const router = useRouter();
+//   const dispatch = useDispatch();
+  const pathname = usePathname();
+  const segments = pathname.split("/");
+  const postId = segments.pop();
+
+  const onBackClick = async () => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      router.push(`/user/post/${postId}`);
+    } else {
+      router.push("/");
+    }
+  };
   return (
     <div className="w-full">
       <section className="flex items-center gap-2 mb-3">
@@ -16,7 +32,7 @@ const UniqueCommentPage = () => {
           strokeWidth={1.5}
           stroke="currentColor"
           className="w-6 h-6 text-white"
-          //   onClick={() => onBackClick()}
+          onClick={() => onBackClick()}
         >
           <path
             strokeLinecap="round"
@@ -24,7 +40,7 @@ const UniqueCommentPage = () => {
             d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
           />
         </svg>
-        <h1 className="text-white text-xl font-semibold">Comment</h1>
+        <h1 className="text-white text-xl font-semibold">Post</h1>
       </section>
       <section>
         <UniqueComment />
