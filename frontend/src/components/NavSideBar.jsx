47fada25f420ed16e6650772a1729c4fd89c6fce
Selena <selena.k.rodriguez@gmail.com>
2024-03-07 15:37:12 -0600

    Changed "Friends" to "Users" to show all non friended users

--- frontend/src/components/NavSideBar.jsx
+++ frontend/src/components/NavSideBar.jsx
@@ -28,10 +28,10 @@ const NavSideBar = () => {
     }
   };
 
-  const handleFriendsClick = () => {
+  const handleAllUsersClick = () => {
     const token = localStorage.getItem("token");
     if (token) {
-      router.push(`/user/${userId}/friends`);
+      router.push(`/user/all-users`);
     } else {
       router.push("/");
     }
@@ -87,7 +87,7 @@ const NavSideBar = () => {
       </div>
       <div
         className="flex items-center gap-2 w-fit hover:border hover:border-hidden hover:rounded-lg hover:text-white hover:bg-slate-400/[0.2] hover:py-1 hover:pl-2 hover:pr-3"
-        onClick={handleFriendsClick}
+        onClick={handleAllUsersClick}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -103,7 +103,7 @@ const NavSideBar = () => {
             d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
           />
         </svg>
-        <p className="text-lg font-semibold text-white/[0.9]">Friends</p>
+        <p className="text-lg font-semibold text-white/[0.9]">Users</p>
       </div>
       <div
         className="flex items-center gap-2 w-fit hover:border hover:border-hidden hover:rounded-lg hover:text-white hover:bg-slate-400/[0.2] hover:py-1 hover:pl-2 hover:pr-3"
