50893f704340a2229e75f328efb386fb2496940d
Selena <selena.k.rodriguez@gmail.com>
2024-03-07 16:42:04 -0600

    Changed the padding and grid layout

--- frontend/src/app/user/all-users/page.jsx
+++ frontend/src/app/user/all-users/page.jsx
@@ -4,13 +4,14 @@ import "../../../styles/globals.css";
 
 const AllUsers = () => {
   return (
-    <div className="grid grid-cols-[1fr_auto] grid-rows-[auto_1fr] gap-x-8 auto-row-auto h-screen w-full bg-slate-700 border border-slate-500 p-3">
-      <section className="col-start-1 row-start-1 flex justify-center">
+    <div className="grid grid-cols-[1fr] grid-rows-[auto_auto_1fr] gap-x-8 auto-row-auto h-fit w-full bg-slate-700 border-2 border-slate-500">
+      <section className="col-start-1 row-start-1 p-3">
         <h1 className="text-white text-xl font-semibold">
           People You May Know:
         </h1>
       </section>
-      <section className="col-start-1 row-start-2 flex justify-center">
+      <div className="w-full border-b-2 border-slate-500 col-start-1 row-start-2"></div>
+      <section className="col-start-1 row-start-3">
         <NonFriendUsers />
       </section>
     </div>
