9c258e998459bae32130bff477be790de2149921
Selena <selena.k.rodriguez@gmail.com>
2024-03-07 13:54:29 -0600

    Added comment model to the comment model for nested comments

--- backend/models/commentModel.js
+++ backend/models/commentModel.js
@@ -6,7 +6,8 @@ const CommentSchema = new Schema(
     body: { type: String, minLength: 1, maxLength: 50, required: true },
     timestamp: { type: Date, default: Date.now },
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+    post: { type: Schema.Types.ObjectId, ref: "Post" },
+    comment: {type: Schema.Types.ObjectId, ref: "Comment"},
     likes: [
       {
         type: Schema.Types.ObjectId,
