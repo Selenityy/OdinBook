078ebc4464860e1481929a80baa603abe1eae17b
Selena <selena.k.rodriguez@gmail.com>
2024-03-13 12:18:24 -0500

    Created a unique comment layout

---
+++ frontend/src/app/user/post/[postId]/comment/[commentId]/layout.jsx
@@ -0,0 +1,11 @@
+
+const UniqueCommentLayout = ({ children }) => {
+
+  return (
+    <div className="w-full">
+      <main className="w-full">{children}</main>
+    </div>
+  );
+};
+
+export default UniqueCommentLayout;
