e813dd53aad964c572d76ee2213cd5e8d8f155df
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-03-18 14:26:35 -0500

    Fixed error with outdated code, replaced with remotePatterns nextjs app router

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,20 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ['localhost'],
+//   },
+// };
+
+// module.exports = nextConfig;
+
 const nextConfig = {
   images: {
-    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+    ],
   },
 };
 
