ed30fd38ec5ca12a19f36aa95812c1d0aa7b0a78
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-03-20 11:57:33 -0500

    Had the header the same flex for all screen sizes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,18 +3,16 @@ import UserProfile from "./UserProfile";
 const Header = () => {
   return (
     <header
-      className="header 
-      xxs:flex xxs:justify-center xxs:items-center xxs:gap-10
-      md:flex md:justify-between md:items-center"
+      className="header flex justify-between items-center"
       id="header-bar"
     >
       <h1
-        className="header text-3xl font-semibold text-white/[0.9] ml-[138px]"
+        className="header text-3xl font-semibold text-white/[0.9]"
         id="logo"
       >
         OdinBook
       </h1>
-      <div className="mr-[138px]">
+      <div className="">
         <UserProfile />
       </div>
     </header>
