7f24f62ef079fbf074a4134665334c3f8f6ee2b0
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-03-21 16:23:02 -0500

    Added small screen size layouts to the page

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -6,22 +6,27 @@ import "../styles/globals.css";
 const Home = () => {
   return (
     <div className="grid 
+    sm:grid-areas-SMhomeLayout 
+    sm:grid-cols-SMhomeLayout 
+    sm:grid-rows-SMhomeLayout 
+    sm:gap-6 
     md:grid-areas-MDhomeLayout 
     md:grid-cols-MDhomeLayout 
     md:grid-rows-MDhomeLayout 
-    md:gap-6
+    md:gap-8
     lg:grid-areas-homeLayout 
     lg:grid-cols-homeLayout 
     lg:grid-rows-homeLayout 
+    lg:gap-0
     h-screen justify-center items-center bg-stone-100">
       <section className="grid-in-odin flex flex-col items-center">
-        <h1 className="md:text-7xl lg:text-8xl font-semibold text-gray-800">OdinBook</h1>
-        <h2 className="md:text-xl lg:text-2xl font-light text-gray-600">
+        <h1 className="sm:text-6xl md:text-7xl lg:text-8xl font-semibold text-gray-800">OdinBook</h1>
+        <h2 className="sm:text-xl md:text-xl lg:text-2xl font-light text-gray-600">
           Connect with others
         </h2>
       </section>
       <div className="grid-in-line border border-gray-500 h-full"></div>
-      <section className="grid-in-login flex flex-col items-center justify-center h-full">
+      <section className="grid-in-login flex flex-col items-center justify-center h-full sm:w-11/12 md:w-full">
         <Login />
         <br></br>
         <div className="flex items-center justify-center space-x-2 md:w-80 lg:w-96 2xl:w-6/12">
