bf42e8e4a8feb738f585c1ae3c025aafac6583af
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-03-27 15:12:16 -0500

    Added a check for username & password errors on all screen sizes

--- frontend/cypress/e2e/login.cy.js
+++ frontend/cypress/e2e/login.cy.js
@@ -51,63 +51,85 @@ describe('login spec', () => {
     });
   });
 
-    // test logging in as 'testuser1' on all viewports
-    viewports.forEach((viewport) => {
-      context(`Logging in as testuser1 on ${viewport.label} screen size`, () => {
-        beforeEach(() => {
-          cy.viewport(viewport.width, viewport.height);
-          cy.visit('/');
-        });
-  
-        it('logs in as testuser1 with password123', () => {
-          cy.get('input#username').type('testuser1');
-          cy.get('input#password').type('password123');
+  // test logging in as 'testuser1' on all viewports
+  viewports.forEach((viewport) => {
+    context(`Logging in as testuser1 on ${viewport.label} screen size`, () => {
+      beforeEach(() => {
+        cy.viewport(viewport.width, viewport.height);
+        cy.visit('/');
+      });
 
-          cy.get('form').submit();
+      it('logs in as testuser1 with password123', () => {
+        cy.get('input#username').type('testuser1');
+        cy.get('input#password').type('password123');
 
-          cy.wait('@login').then(() => {
-            cy.url().should('include', '/user');
-            if (!skipDivCheckSizes.includes(viewport.label)) {
-              cy.get('div').contains('testuser1').should('be.visible');
-            }
-          });
+        cy.get('form').submit();
+
+        cy.wait('@login').then(() => {
+          cy.url().should('include', '/user');
+          if (!skipDivCheckSizes.includes(viewport.label)) {
+            cy.get('div').contains('testuser1').should('be.visible');
+          }
         });
-  
+      });
+
+    });
+  });
+
+  // test navigating to the sign up back when create account button is clicked on all viewports
+  viewports.forEach((viewport) => {
+    context(`Navigate to sign up page on ${viewport.label} screen size`, () => {
+      beforeEach(() => {
+        cy.viewport(viewport.width, viewport.height);
+        cy.visit('/');
+      });
+
+      // go to sign up page when create account button is clicked
+      it('navigates to the signup page', () => {
+        cy.findByRole('button', {name: /Create Account/i}).click();
+
+        cy.wait('@signup').then(() => {
+          cy.url().should('include', '/signup');
+        });
+      });
+
+    });
+  });
+
+  // test incorrect username on all viewports
+  viewports.forEach((viewport) => {
+    context(`Incorrect username error on ${viewport.label} screen size`, () => {
+      beforeEach(() => {
+        cy.viewport(viewport.width, viewport.height);
+        cy.visit('/');
+      });
+
+    // incorrect username
+    it.only('display error message for incorrect username', () => {
+      cy.get('input#username').type('testuser');
+      cy.get('input#password').type('password123');
+
+      cy.get('form').submit();
+
+      cy.wait('@loginFail').then(() => {
+        cy.get('div').contains('*Username or Password is incorrect.');
       });
     });
 
-  
-  // go to sign up page when create account button is clicked
-  it('navigates to the signup page', () => {
-    cy.findByRole('button', {name: /Create Account/i}).click();
+    // incorrect password
+    it.only('display error message for incorrect password', () => {
+      cy.get('input#username').type('testuser1');
+      cy.get('input#password').type('password1234');
 
-    cy.wait('@signup').then(() => {
-      cy.url().should('include', '/signup');
-    })
-  })
+      cy.get('form').submit();
 
-  // incorrect username
-  it('display error message for incorrect username', () => {
-    cy.get('input#username').type('testuser');
-    cy.get('input#password').type('password123');
+      cy.wait('@loginFail').then(() => {
+        cy.get('div').contains('*Username or Password is incorrect.');
+      });
+    });
 
-    cy.get('form').submit();
+    });
+  });
 
-    cy.wait('@loginFail').then(() => {
-      cy.get('div').contains('*Username or Password is incorrect.');
-    })
-  })
-
-  // incorrect password
-  it('display error message for incorrect username', () => {
-    cy.get('input#username').type('testuser1');
-    cy.get('input#password').type('password1234');
-
-    cy.get('form').submit();
-
-    cy.wait('@loginFail').then(() => {
-      cy.get('div').contains('*Username or Password is incorrect.');
-    })
-  })
   // check log in for different screen sizes
 })
