0af5d68917eb4a33c6ee93629444e2de061a38ef
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-03-29 15:18:50 -0500

    Saving

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,10 +1,12 @@
+const { configurePlugin } = require("cypress-mongodb");
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3001',
+    baseUrl: "http://localhost:3001",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      configurePlugin(on);
     },
   },
 });
