73e97568672a14b0e679667a4265ae14fac0ccdd
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-04-06 14:54:56 -0500

    Add debug log for username

--- backend/helpers/passport.js
+++ backend/helpers/passport.js
@@ -10,6 +10,7 @@ const User = require("../models/userModel");
 passport.use(
   new LocalStrategy(async (username, password, done) => {
     try {
+      console.log(`Username is: ${username}`)
       const user = await User.findOne({ username: username });
       if (!user) {
         return done(null, false, { message: "Incorrect username" });
